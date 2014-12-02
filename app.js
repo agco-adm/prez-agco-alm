2df2c7cb5b07ecc8114151ee4b5db2c1b6c59b7a
Diogo Lucas <diogo.lucas@gmail.com>
2014-12-02 10:12:12 -0800

    Express now serves static content, should work with port configuration as well...

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express')
+
+var app = express()
+
+app.use(express.static(__dirname))
+
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), function() {
+	console.log("Express server listening on port " + app.get('port'))
+})
+
